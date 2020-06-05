a33ac10244cbf722bbcdee8996ae16a6ddf72ad8
DogBar24 <62043517+DogBar24@users.noreply.github.com>
2020-06-06 01:11:24 +0700

    Customers
    program add / Remove / ShowData
    Customers

---
+++ Test_Al/Test_Al/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_Al
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            arrayList.Add(txtAdd.Text);
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            arrayList.Remove(txtRemove.Text);
+        }
+
+        private void Show_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string data in arrayList)
+            {
+                MessageBox.Show(data);
+            }
+            MessageBox.Show("Customers Name = " + arrayList.Count.ToString());
+        }
+    }
+}
